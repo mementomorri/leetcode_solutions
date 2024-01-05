49d440845184f63b7f9be787261d7817de12bbc0
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-01-05 03:55:46 +0300

    add solutions from books

---
+++ SICP_JS/chapter_1/exercise_1_2.js
@@ -0,0 +1,7 @@
+// exercise 1.2
+const numerator = 5 + 4 + (2 - (3 - (6 + 0.20)))
+const divisor = 3 * (6 -2 ) * (2 - 7)
+
+console.log(numerator)
+console.log(divisor)
+console.log(numerator/divisor)  // -0.23666666666666
