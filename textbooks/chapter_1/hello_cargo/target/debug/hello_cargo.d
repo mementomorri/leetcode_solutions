80611f12d7540a9cf8d6b5201ede4f50b22705ec
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-01-25 13:22:31 +0300

    Delete textbooks/chapter_1 directory

--- textbooks/chapter_1/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/media/data/python_projects_home/leetcode_archive/textbooks/the_rust_book/chapter_1/hello_cargo/target/debug/hello_cargo: /media/data/python_projects_home/leetcode_archive/textbooks/the_rust_book/chapter_1/hello_cargo/src/main.rs
--- textbooks/chapter_1/hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/media/data/python_projects_home/leetcode_archive/textbooks/the_rust_book/chapter_1/hello_cargo/target/release/hello_cargo: /media/data/python_projects_home/leetcode_archive/textbooks/the_rust_book/chapter_1/hello_cargo/src/main.rs
