321b24896bead733efb18c4f6b4991afc391417b
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-05-06 01:04:08 +0300

    Create 23_Merge_k_Sorted_Lists.py

---
+++ leetcode/hard/23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,81 @@
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 104.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def merge(self, left: ListNode, right: ListNode) -> ListNode:
+        tail = ListNode(-1)
+        head = tail
+        
+        while left and right:
+            if left.val < right.val:
+                head.next = left
+                left = left.next
+            else:
+                head.next = right
+                right = right.next
+            head = head.next
+
+        if left:
+            head.next = left
+        if right:
+            head.next = right
+
+        return tail.next
+    
+    def mergeSort(self, lists: List[ListNode], start: int, end: int) -> ListNode:
+        if start == end:
+            return lists[start]
+
+        mid = start + (end - start) // 2
+        left = self.mergeSort(lists, start, mid)
+        right = self.mergeSort(lists, mid + 1, end)
+
+        return self.merge(left, right)
+    
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
+        return self.mergeSort(lists, 0, len(lists) - 1)
