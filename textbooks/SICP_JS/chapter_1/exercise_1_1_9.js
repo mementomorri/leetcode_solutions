f4504b18e43dfb0bcaa6e72d2173a079716bd744
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-08-12 19:08:34 +0300

    Add files via upload

---
+++ textbooks/SICP_JS/chapter_1/exercise_1_1_9.js
@@ -0,0 +1,9 @@
+// exercise 1.1.9
+//
+// First function is recursive and so is the process.
+// We call function `plus` recursively and keep on stacking
+// incremention of result.
+//
+// Second function is recursive, but process is iterative.
+// We increment variables first, and then make tail-recirsive call,
+// which makes it iterative.
