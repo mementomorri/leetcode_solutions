e4a43ccb6edad85e6fb8dfaf96d91ad6ff70c737
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-11-21 13:56:32 +0300

    Add files via upload

---
+++ leetcode/medium/2257_Count_Unguarded_Cells_in_the_Grid.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        def mark_guarded(r, c):
+            for row in range(r + 1, m):
+                if grid[row][c] in (1, 2):
+                    break
+                grid[row][c] = 3
+            for row in reversed(range(0, r)):
+                if grid[row][c] in (1, 2):
+                    break
+                grid[row][c] = 3
+            for col in range(c + 1, n):
+                if grid[r][col] in (1, 2):
+                    break
+                grid[r][col] = 3
+            for col in reversed(range(0, c)):
+                if grid[r][col] in (1, 2):
+                    break
+                grid[r][col] = 3
+
+        grid = [[0] * n for _ in range(m)]
+
+        for r, c in guards:
+            grid[r][c] = 1
+        for r, c in walls:
+            grid[r][c] = 2
+
+        for r, c in guards:
+            mark_guarded(r, c)
+
+        res = 0
+        for row in grid:
+            for n in row:
+                if n == 0:
+                    res += 1
+        return res
