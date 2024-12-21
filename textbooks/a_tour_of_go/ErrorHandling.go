6a36bb44460b6c6a8179e170e8aeff0cba5fd5a9
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2024-12-21 19:31:24 +0300

    Add files via upload

---
+++ textbooks/a_tour_of_go/ErrorHandling.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := x
+	var prevZ float64
+	for {
+		prevZ = z
+		z -= (z*z - x) / (2 * z)
+		if math.Abs(z-prevZ) < 1e-10 {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
