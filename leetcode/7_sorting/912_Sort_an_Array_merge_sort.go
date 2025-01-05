e93c299bcfc58a8b89e8e0746e757f22800aa44e
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2025-01-05 19:11:56 +0300

    Add files via upload

---
+++ leetcode/7_sorting/912_Sort_an_Array_merge_sort.go
@@ -0,0 +1,38 @@
+func sortArray(nums []int) []int {
+	return mergeSort(nums)
+}
+
+func mergeSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	left := mergeSort(nums[:(len(nums)+1)/2])
+	right := mergeSort(nums[(len(nums)+1)/2:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, len(left)+len(right))
+	i := 0
+	j := 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result[i+j] = left[i]
+			i++
+		} else {
+			result[i+j] = right[j]
+			j++
+		}
+	}
+	for i < len(left) {
+		result[i+j] = left[i]
+		i++
+	}
+	for j < len(right) {
+		result[i+j] = right[j]
+		j++
+	}
+	return result
+}
