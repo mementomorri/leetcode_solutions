0236b29d0b223405ddcb2ae1742b2f966c4a7c59
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2025-01-10 09:56:35 +0300

    Add files via upload

--- leetcode/14_trees/102_Binary_Tree_Level_Order_Traversal.py
+++ leetcode/14_trees/102_Binary_Tree_Level_Order_Traversal.py
@@ -3,6 +3,26 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
+        res = []
+        q = []
+        if root:
+            q.append(root)
+
+        level = 0
+        while q:
+            res.append([])
+            q_len = len(q)
+            for _ in range(q_len):
+                cur = q.pop(0)
+                res[level].append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            level += 1
+
+        return res
