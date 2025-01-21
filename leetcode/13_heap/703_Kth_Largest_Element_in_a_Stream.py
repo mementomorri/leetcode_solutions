bdaab74ae640f0ea6844f373f9156c9a430023dc
Alexey Karsten <44745297+mementomorri@users.noreply.github.com>
2025-01-21 08:16:29 +0300

    Add files via upload

--- leetcode/13_heap/703_Kth_Largest_Element_in_a_Stream.py
+++ leetcode/13_heap/703_Kth_Largest_Element_in_a_Stream.py
@@ -1,56 +1,82 @@
-"""
-Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
-
-Implement KthLargest class:
-
-    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
-    int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
-
-
-
-Example 1:
-
-Input
-["KthLargest", "add", "add", "add", "add", "add"]
-[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
-Output
-[null, 4, 5, 5, 8, 8]
-
-Explanation
-KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
-kthLargest.add(3);   // return 4
-kthLargest.add(5);   // return 5
-kthLargest.add(10);  // return 5
-kthLargest.add(9);   // return 8
-kthLargest.add(4);   // return 8
-
-
-
-Constraints:
-
-    1 <= k <= 104
-    0 <= nums.length <= 104
-    -104 <= nums[i] <= 104
-    -104 <= val <= 104
-    At most 104 calls will be made to add.
-    It is guaranteed that there will be at least k elements in the array when you search for the kth element.
-"""
-
-
 class KthLargest:
-    def __init__(self, k: int, nums: List[int]):
+    def __init__(self, k: int, nums: list[int]):
         self.k = k
-        self.min_heap = []
-        for num in nums:
-            self.add(num)
+        self.heap = [0]
+        self.heapify(nums)
+        while len(self.heap) > k + 1:
+            self.pop()
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.min_heap, val)
-        if len(self.min_heap) > self.k:
-            heapq.heappop(self.min_heap)
-        return self.min_heap[0]
+        self.heap.append(val)
+        i = len(self.heap) - 1
+
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
+            i //= 2
+
+        while len(self.heap) > self.k + 1:
+            self.pop()
+        return self.heap[1]
+
+    def percolate_down(self, i=1):
+        while 2 * i < len(self.heap):
+            if (
+                2 * i + 1 < len(self.heap)
+                and self.heap[2 * i + 1] < self.heap[2 * i]
+                and self.heap[i] > self.heap[2 * i + 1]
+            ):
+                # Swap right child
+                self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
+                i = 2 * i + 1
+            elif self.heap[i] > self.heap[2 * i]:
+                # Swap left child
+                self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
+                i = 2 * i
+            else:
+                break
+
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+
+        res = self.heap[1]
+        # Move last value to root
+        self.heap[1] = self.heap.pop()
+        self.percolate_down()
+
+        return res
+
+    def top(self):
+        if len(self.heap) > 1:
+            return self.heap[1]
+        return None
+
+    def heapify(self, arr):
+        # 0-th position is moved to the end
+
+        self.heap.extend(arr)
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            self.percolate_down(cur)
+            cur -= 1
 
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+if __name__ == '__main__':
+    test_kl = KthLargest(3, [4, 5, 8, 2])
+    print(test_kl.heap)
+    print(test_kl.add(3), "= 4")
+    print(test_kl.heap)
+    print(test_kl.add(5), "= 5")
+    print(test_kl.heap)
+    print(test_kl.add(10), "= 5")
+    print(test_kl.heap)
+    print(test_kl.add(9), "= 8")
+    print(test_kl.heap)
+    print(test_kl.add(4), "= 8")
+    print(test_kl.heap)
